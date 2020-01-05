fe9caf945fb576473cf4e99ab60a7d8a579f83cc
thomas9911 <thomastimmer11@hotmail.com>
2020-01-05 15:04:45 +0100

    description function on error is deprecated

--- serde_rustler/src/error.rs
+++ serde_rustler/src/error.rs
@@ -7,118 +7,118 @@ quick_error! {
     #[derive(Debug)]
     pub enum Error {
         DeserializationError(err: String) {
-            description(err)
+            display("{}", err)
         }
         TypeHintsRequired {
-            description("Cannot deserialize any, type hints are required")
+            display("Cannot deserialize any, type hints are required")
         }
         InvalidAtom {
-            description("Failed to deserialize atom")
+            display("Failed to deserialize atom")
         }
         InvalidBoolean {
-            description("Failed to deserialize boolean")
+            display("Failed to deserialize boolean")
         }
         InvalidNumber {
-            description("Failed to deserialize number")
+            display("Failed to deserialize number")
         }
         InvalidStringable {
-            description("Failed to deserialize term as an &str")
+            display("Failed to deserialize term as an &str")
         }
         InvalidList {
-            description("Failed to deserialize list")
+            display("Failed to deserialize list")
         }
         InvalidTuple {
-            description("Failed to deserialize tuple")
+            display("Failed to deserialize tuple")
         }
         InvalidSequenceElement {
-            description("Failed to deserialize sequence element")
+            display("Failed to deserialize sequence element")
         }
 
         ExpectedAtom {
-            description("Expected to deserialize atom")
+            display("Expected to deserialize atom")
         }
         ExpectedBoolean {
-            description("Expected to deserialize boolean")
+            display("Expected to deserialize boolean")
         }
         ExpectedBinary {
-            description("Expected to deserialize binary")
+            display("Expected to deserialize binary")
         }
         ExpectedNumber {
-            description("Expected to deserialize number")
+            display("Expected to deserialize number")
         }
         ExpectedChar {
-            description("Expected to deserialize char")
+            display("Expected to deserialize char")
         }
         ExpectedStringable {
-            description("Expected to deserialize a UTF-8 stringable term")
+            display("Expected to deserialize a UTF-8 stringable term")
         }
         ExpectedNil {
-            description("Expected to deserialize nil")
+            display("Expected to deserialize nil")
         }
         ExpectedList {
-            description("Expected to deserialize list")
+            display("Expected to deserialize list")
         }
         ExpectedTuple {
-            description("Expected to deserialize tuple")
+            display("Expected to deserialize tuple")
         }
         ExpectedEnum {
-            description("Expected to deserialize enum")
+            display("Expected to deserialize enum")
         }
         ExpectedMap {
-            description("Expected to deserialize map")
+            display("Expected to deserialize map")
         }
         ExpectedStruct {
-            description("Expected to deserialize struct")
+            display("Expected to deserialize struct")
         }
         ExpectedStructName {
-            description("Expected to deserialize struct name")
+            display("Expected to deserialize struct name")
         }
         ExpectedStructValue {
-            description("Expected to deserialize struct value")
+            display("Expected to deserialize struct value")
         }
         ExpectedUnitVariant {
-            description("Expected to deserialize unit variant")
+            display("Expected to deserialize unit variant")
         }
         ExpectedNewtypeStruct {
-            description("Expected to deserialize newtype struct tuple")
+            display("Expected to deserialize newtype struct tuple")
         }
         ExpectedNewtypeVariant {
-            description("Expected to deserialize newtype variant")
+            display("Expected to deserialize newtype variant")
         }
         ExpectedTupleVariant {
-            description("Expected to deserialize tuple variant")
+            display("Expected to deserialize tuple variant")
         }
         ExpectedStructVariant {
-            description("Expected to deserialize struct variant")
+            display("Expected to deserialize struct variant")
         }
 
         SerializationError(err: String) {
-            description(err)
+            display("{}", err)
         }
         InvalidVariantName {
-            description("Failed to serialize variant to atom or string")
+            display("Failed to serialize variant to atom or string")
         }
         InvalidStructName {
-            description("Failed to serialize struct name to atom or string")
+            display("Failed to serialize struct name to atom or string")
         }
         InvalidBinary {
-            description("Failed to serialize binary")
+            display("Failed to serialize binary")
         }
         InvalidMap {
-            description("Failed to serialize map to NIF map")
+            display("Failed to serialize map to NIF map")
         }
         InvalidStruct {
-            description("Failed to serialize struct to NIF struct")
+            display("Failed to serialize struct to NIF struct")
         }
         InvalidStructKey {
-            description("Failed to serialize struct key")
+            display("Failed to serialize struct key")
         }
     }
 }
 
 impl From<Error> for NifError {
     fn from(err: Error) -> NifError {
-        NifError::RaiseTerm(Box::new(String::from(err.description())))
+        NifError::RaiseTerm(Box::new(String::from(err.to_string())))
     }
 }
 
