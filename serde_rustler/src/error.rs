500457db9e81b58443d3f0f7181462bc6b6d807c
Praveen Perera <ppraveen25@gmail.com>
2020-03-05 12:52:09 -0500

    Fix clippy warnings

--- serde_rustler/src/error.rs
+++ serde_rustler/src/error.rs
@@ -1,7 +1,7 @@
 use quick_error::quick_error;
 use rustler::Error as NifError;
 use serde::{de, ser};
-use std::{error::Error as StdError, fmt::Display};
+use std::fmt::Display;
 
 quick_error! {
     #[derive(Debug)]
@@ -118,7 +118,7 @@ quick_error! {
 
 impl From<Error> for NifError {
     fn from(err: Error) -> NifError {
-        NifError::RaiseTerm(Box::new(String::from(err.to_string())))
+        NifError::RaiseTerm(Box::new(err.to_string()))
     }
 }
 
